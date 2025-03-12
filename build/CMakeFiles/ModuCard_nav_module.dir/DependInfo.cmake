2ae321edded9cc07590b128cae633c15c68fdd2d
patryk LapUbuntu <dodpat02@gmail.com>
2025-03-12 18:56:50 +0100

    init

---
+++ build/CMakeFiles/ModuCard_nav_module.dir/DependInfo.cmake
@@ -0,0 +1,94 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/home/lemonx/it/ModuCard-nav-module-software/startup_stm32h750xx.s" "/home/lemonx/it/ModuCard-nav-module-software/build/CMakeFiles/ModuCard_nav_module.dir/startup_stm32h750xx.s.obj"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "DEBUG"
+  "STM32H750xx"
+  "USE_HAL_DRIVER"
+  "USE_PWR_LDO_SUPPLY"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "/home/lemonx/it/ModuCard-nav-module-software/cmake/stm32cubemx/../../Core/Inc"
+  "/home/lemonx/it/ModuCard-nav-module-software/cmake/stm32cubemx/../../Drivers/STM32H7xx_HAL_Driver/Inc"
+  "/home/lemonx/it/ModuCard-nav-module-software/cmake/stm32cubemx/../../Drivers/STM32H7xx_HAL_Driver/Inc/Legacy"
+  "/home/lemonx/it/ModuCard-nav-module-software/cmake/stm32cubemx/../../Middlewares/Third_Party/FreeRTOS/Source/include"
+  "/home/lemonx/it/ModuCard-nav-module-software/cmake/stm32cubemx/../../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2"
+  "/home/lemonx/it/ModuCard-nav-module-software/cmake/stm32cubemx/../../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F"
+  "/home/lemonx/it/ModuCard-nav-module-software/cmake/stm32cubemx/../../Drivers/CMSIS/Device/ST/STM32H7xx/Include"
+  "/home/lemonx/it/ModuCard-nav-module-software/cmake/stm32cubemx/../../Drivers/CMSIS/Include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/lemonx/it/ModuCard-nav-module-software/Core/Src/freertos.c" "CMakeFiles/ModuCard_nav_module.dir/Core/Src/freertos.c.obj" "gcc" "CMakeFiles/ModuCard_nav_module.dir/Core/Src/freertos.c.obj.d"
+  "/home/lemonx/it/ModuCard-nav-module-software/Core/Src/main.c" "CMakeFiles/ModuCard_nav_module.dir/Core/Src/main.c.obj" "gcc" "CMakeFiles/ModuCard_nav_module.dir/Core/Src/main.c.obj.d"
+  "/home/lemonx/it/ModuCard-nav-module-software/Core/Src/stm32h7xx_hal_msp.c" "CMakeFiles/ModuCard_nav_module.dir/Core/Src/stm32h7xx_hal_msp.c.obj" "gcc" "CMakeFiles/ModuCard_nav_module.dir/Core/Src/stm32h7xx_hal_msp.c.obj.d"
+  "/home/lemonx/it/ModuCard-nav-module-software/Core/Src/stm32h7xx_hal_timebase_tim.c" "CMakeFiles/ModuCard_nav_module.dir/Core/Src/stm32h7xx_hal_timebase_tim.c.obj" "gcc" "CMakeFiles/ModuCard_nav_module.dir/Core/Src/stm32h7xx_hal_timebase_tim.c.obj.d"
+  "/home/lemonx/it/ModuCard-nav-module-software/Core/Src/stm32h7xx_it.c" "CMakeFiles/ModuCard_nav_module.dir/Core/Src/stm32h7xx_it.c.obj" "gcc" "CMakeFiles/ModuCard_nav_module.dir/Core/Src/stm32h7xx_it.c.obj.d"
+  "/home/lemonx/it/ModuCard-nav-module-software/Core/Src/syscalls.c" "CMakeFiles/ModuCard_nav_module.dir/Core/Src/syscalls.c.obj" "gcc" "CMakeFiles/ModuCard_nav_module.dir/Core/Src/syscalls.c.obj.d"
+  "/home/lemonx/it/ModuCard-nav-module-software/Core/Src/sysmem.c" "CMakeFiles/ModuCard_nav_module.dir/Core/Src/sysmem.c.obj" "gcc" "CMakeFiles/ModuCard_nav_module.dir/Core/Src/sysmem.c.obj.d"
+  "/home/lemonx/it/ModuCard-nav-module-software/Core/Src/system_stm32h7xx.c" "CMakeFiles/ModuCard_nav_module.dir/Core/Src/system_stm32h7xx.c.obj" "gcc" "CMakeFiles/ModuCard_nav_module.dir/Core/Src/system_stm32h7xx.c.obj.d"
+  "/home/lemonx/it/ModuCard-nav-module-software/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal.c" "CMakeFiles/ModuCard_nav_module.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal.c.obj" "gcc" "CMakeFiles/ModuCard_nav_module.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal.c.obj.d"
+  "/home/lemonx/it/ModuCard-nav-module-software/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_adc.c" "CMakeFiles/ModuCard_nav_module.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_adc.c.obj" "gcc" "CMakeFiles/ModuCard_nav_module.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_adc.c.obj.d"
+  "/home/lemonx/it/ModuCard-nav-module-software/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_adc_ex.c" "CMakeFiles/ModuCard_nav_module.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_adc_ex.c.obj" "gcc" "CMakeFiles/ModuCard_nav_module.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_adc_ex.c.obj.d"
+  "/home/lemonx/it/ModuCard-nav-module-software/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_cortex.c" "CMakeFiles/ModuCard_nav_module.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_cortex.c.obj" "gcc" "CMakeFiles/ModuCard_nav_module.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_cortex.c.obj.d"
+  "/home/lemonx/it/ModuCard-nav-module-software/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_dac.c" "CMakeFiles/ModuCard_nav_module.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_dac.c.obj" "gcc" "CMakeFiles/ModuCard_nav_module.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_dac.c.obj.d"
+  "/home/lemonx/it/ModuCard-nav-module-software/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_dac_ex.c" "CMakeFiles/ModuCard_nav_module.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_dac_ex.c.obj" "gcc" "CMakeFiles/ModuCard_nav_module.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_dac_ex.c.obj.d"
+  "/home/lemonx/it/ModuCard-nav-module-software/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_dma.c" "CMakeFiles/ModuCard_nav_module.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_dma.c.obj" "gcc" "CMakeFiles/ModuCard_nav_module.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_dma.c.obj.d"
+  "/home/lemonx/it/ModuCard-nav-module-software/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_dma_ex.c" "CMakeFiles/ModuCard_nav_module.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_dma_ex.c.obj" "gcc" "CMakeFiles/ModuCard_nav_module.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_dma_ex.c.obj.d"
+  "/home/lemonx/it/ModuCard-nav-module-software/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_exti.c" "CMakeFiles/ModuCard_nav_module.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_exti.c.obj" "gcc" "CMakeFiles/ModuCard_nav_module.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_exti.c.obj.d"
+  "/home/lemonx/it/ModuCard-nav-module-software/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_fdcan.c" "CMakeFiles/ModuCard_nav_module.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_fdcan.c.obj" "gcc" "CMakeFiles/ModuCard_nav_module.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_fdcan.c.obj.d"
+  "/home/lemonx/it/ModuCard-nav-module-software/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_flash.c" "CMakeFiles/ModuCard_nav_module.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_flash.c.obj" "gcc" "CMakeFiles/ModuCard_nav_module.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_flash.c.obj.d"
+  "/home/lemonx/it/ModuCard-nav-module-software/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_flash_ex.c" "CMakeFiles/ModuCard_nav_module.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_flash_ex.c.obj" "gcc" "CMakeFiles/ModuCard_nav_module.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_flash_ex.c.obj.d"
+  "/home/lemonx/it/ModuCard-nav-module-software/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_gpio.c" "CMakeFiles/ModuCard_nav_module.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_gpio.c.obj" "gcc" "CMakeFiles/ModuCard_nav_module.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_gpio.c.obj.d"
+  "/home/lemonx/it/ModuCard-nav-module-software/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_hsem.c" "CMakeFiles/ModuCard_nav_module.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_hsem.c.obj" "gcc" "CMakeFiles/ModuCard_nav_module.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_hsem.c.obj.d"
+  "/home/lemonx/it/ModuCard-nav-module-software/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_i2c.c" "CMakeFiles/ModuCard_nav_module.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_i2c.c.obj" "gcc" "CMakeFiles/ModuCard_nav_module.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_i2c.c.obj.d"
+  "/home/lemonx/it/ModuCard-nav-module-software/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_i2c_ex.c" "CMakeFiles/ModuCard_nav_module.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_i2c_ex.c.obj" "gcc" "CMakeFiles/ModuCard_nav_module.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_i2c_ex.c.obj.d"
+  "/home/lemonx/it/ModuCard-nav-module-software/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_mdma.c" "CMakeFiles/ModuCard_nav_module.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_mdma.c.obj" "gcc" "CMakeFiles/ModuCard_nav_module.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_mdma.c.obj.d"
+  "/home/lemonx/it/ModuCard-nav-module-software/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_pcd.c" "CMakeFiles/ModuCard_nav_module.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_pcd.c.obj" "gcc" "CMakeFiles/ModuCard_nav_module.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_pcd.c.obj.d"
+  "/home/lemonx/it/ModuCard-nav-module-software/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_pcd_ex.c" "CMakeFiles/ModuCard_nav_module.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_pcd_ex.c.obj" "gcc" "CMakeFiles/ModuCard_nav_module.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_pcd_ex.c.obj.d"
+  "/home/lemonx/it/ModuCard-nav-module-software/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_pwr.c" "CMakeFiles/ModuCard_nav_module.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_pwr.c.obj" "gcc" "CMakeFiles/ModuCard_nav_module.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_pwr.c.obj.d"
+  "/home/lemonx/it/ModuCard-nav-module-software/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_pwr_ex.c" "CMakeFiles/ModuCard_nav_module.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_pwr_ex.c.obj" "gcc" "CMakeFiles/ModuCard_nav_module.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_pwr_ex.c.obj.d"
+  "/home/lemonx/it/ModuCard-nav-module-software/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_qspi.c" "CMakeFiles/ModuCard_nav_module.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_qspi.c.obj" "gcc" "CMakeFiles/ModuCard_nav_module.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_qspi.c.obj.d"
+  "/home/lemonx/it/ModuCard-nav-module-software/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_rcc.c" "CMakeFiles/ModuCard_nav_module.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_rcc.c.obj" "gcc" "CMakeFiles/ModuCard_nav_module.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_rcc.c.obj.d"
+  "/home/lemonx/it/ModuCard-nav-module-software/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_rcc_ex.c" "CMakeFiles/ModuCard_nav_module.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_rcc_ex.c.obj" "gcc" "CMakeFiles/ModuCard_nav_module.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_rcc_ex.c.obj.d"
+  "/home/lemonx/it/ModuCard-nav-module-software/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_spi.c" "CMakeFiles/ModuCard_nav_module.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_spi.c.obj" "gcc" "CMakeFiles/ModuCard_nav_module.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_spi.c.obj.d"
+  "/home/lemonx/it/ModuCard-nav-module-software/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_spi_ex.c" "CMakeFiles/ModuCard_nav_module.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_spi_ex.c.obj" "gcc" "CMakeFiles/ModuCard_nav_module.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_spi_ex.c.obj.d"
+  "/home/lemonx/it/ModuCard-nav-module-software/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_tim.c" "CMakeFiles/ModuCard_nav_module.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_tim.c.obj" "gcc" "CMakeFiles/ModuCard_nav_module.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_tim.c.obj.d"
+  "/home/lemonx/it/ModuCard-nav-module-software/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_tim_ex.c" "CMakeFiles/ModuCard_nav_module.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_tim_ex.c.obj" "gcc" "CMakeFiles/ModuCard_nav_module.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_tim_ex.c.obj.d"
+  "/home/lemonx/it/ModuCard-nav-module-software/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_uart.c" "CMakeFiles/ModuCard_nav_module.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_uart.c.obj" "gcc" "CMakeFiles/ModuCard_nav_module.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_uart.c.obj.d"
+  "/home/lemonx/it/ModuCard-nav-module-software/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_uart_ex.c" "CMakeFiles/ModuCard_nav_module.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_uart_ex.c.obj" "gcc" "CMakeFiles/ModuCard_nav_module.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_hal_uart_ex.c.obj.d"
+  "/home/lemonx/it/ModuCard-nav-module-software/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_ll_delayblock.c" "CMakeFiles/ModuCard_nav_module.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_ll_delayblock.c.obj" "gcc" "CMakeFiles/ModuCard_nav_module.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_ll_delayblock.c.obj.d"
+  "/home/lemonx/it/ModuCard-nav-module-software/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_ll_usb.c" "CMakeFiles/ModuCard_nav_module.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_ll_usb.c.obj" "gcc" "CMakeFiles/ModuCard_nav_module.dir/Drivers/STM32H7xx_HAL_Driver/Src/stm32h7xx_ll_usb.c.obj.d"
+  "/home/lemonx/it/ModuCard-nav-module-software/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2/cmsis_os2.c" "CMakeFiles/ModuCard_nav_module.dir/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2/cmsis_os2.c.obj" "gcc" "CMakeFiles/ModuCard_nav_module.dir/Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2/cmsis_os2.c.obj.d"
+  "/home/lemonx/it/ModuCard-nav-module-software/Middlewares/Third_Party/FreeRTOS/Source/croutine.c" "CMakeFiles/ModuCard_nav_module.dir/Middlewares/Third_Party/FreeRTOS/Source/croutine.c.obj" "gcc" "CMakeFiles/ModuCard_nav_module.dir/Middlewares/Third_Party/FreeRTOS/Source/croutine.c.obj.d"
+  "/home/lemonx/it/ModuCard-nav-module-software/Middlewares/Third_Party/FreeRTOS/Source/event_groups.c" "CMakeFiles/ModuCard_nav_module.dir/Middlewares/Third_Party/FreeRTOS/Source/event_groups.c.obj" "gcc" "CMakeFiles/ModuCard_nav_module.dir/Middlewares/Third_Party/FreeRTOS/Source/event_groups.c.obj.d"
+  "/home/lemonx/it/ModuCard-nav-module-software/Middlewares/Third_Party/FreeRTOS/Source/list.c" "CMakeFiles/ModuCard_nav_module.dir/Middlewares/Third_Party/FreeRTOS/Source/list.c.obj" "gcc" "CMakeFiles/ModuCard_nav_module.dir/Middlewares/Third_Party/FreeRTOS/Source/list.c.obj.d"
+  "/home/lemonx/it/ModuCard-nav-module-software/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/port.c" "CMakeFiles/ModuCard_nav_module.dir/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/port.c.obj" "gcc" "CMakeFiles/ModuCard_nav_module.dir/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM4F/port.c.obj.d"
+  "/home/lemonx/it/ModuCard-nav-module-software/Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/heap_4.c" "CMakeFiles/ModuCard_nav_module.dir/Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/heap_4.c.obj" "gcc" "CMakeFiles/ModuCard_nav_module.dir/Middlewares/Third_Party/FreeRTOS/Source/portable/MemMang/heap_4.c.obj.d"
+  "/home/lemonx/it/ModuCard-nav-module-software/Middlewares/Third_Party/FreeRTOS/Source/queue.c" "CMakeFiles/ModuCard_nav_module.dir/Middlewares/Third_Party/FreeRTOS/Source/queue.c.obj" "gcc" "CMakeFiles/ModuCard_nav_module.dir/Middlewares/Third_Party/FreeRTOS/Source/queue.c.obj.d"
+  "/home/lemonx/it/ModuCard-nav-module-software/Middlewares/Third_Party/FreeRTOS/Source/stream_buffer.c" "CMakeFiles/ModuCard_nav_module.dir/Middlewares/Third_Party/FreeRTOS/Source/stream_buffer.c.obj" "gcc" "CMakeFiles/ModuCard_nav_module.dir/Middlewares/Third_Party/FreeRTOS/Source/stream_buffer.c.obj.d"
+  "/home/lemonx/it/ModuCard-nav-module-software/Middlewares/Third_Party/FreeRTOS/Source/tasks.c" "CMakeFiles/ModuCard_nav_module.dir/Middlewares/Third_Party/FreeRTOS/Source/tasks.c.obj" "gcc" "CMakeFiles/ModuCard_nav_module.dir/Middlewares/Third_Party/FreeRTOS/Source/tasks.c.obj.d"
+  "/home/lemonx/it/ModuCard-nav-module-software/Middlewares/Third_Party/FreeRTOS/Source/timers.c" "CMakeFiles/ModuCard_nav_module.dir/Middlewares/Third_Party/FreeRTOS/Source/timers.c.obj" "gcc" "CMakeFiles/ModuCard_nav_module.dir/Middlewares/Third_Party/FreeRTOS/Source/timers.c.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/ModuCard_nav_module.dir/compiler_depend.ts
@@ -0,0 +1,2 @@
+# CMAKE generated file: DO NOT EDIT!
+# Timestamp file for compiler generated dependencies management for ModuCard_nav_module.
