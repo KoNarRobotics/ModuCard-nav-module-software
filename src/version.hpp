1408b97fbf969d811de6d6a915c4e3790052fd13
Patryk Dudizński <dodpat02@gmail.com>
2025-05-18 22:21:01 +0200

    remioved dyanmic cast

--- src/version.hpp
+++ src/version.hpp
@@ -2,11 +2,11 @@
 #define VERSION_HPP
 
 #define VERSION_MAJOR 0
-#define VERSION_MINOR 77
-#define VERSION_BUILD 4
+#define VERSION_MINOR 78
+#define VERSION_BUILD 0
 
 // for easy find  in a .bin file
-#define VERSION_DEF_BUILD_STRING_DEF "build_version:0.77.4"
+#define VERSION_DEF_BUILD_STRING_DEF "build_version:0.78.0"
 static const char* VERSION_DEF_BUILD_STRING = VERSION_DEF_BUILD_STRING_DEF;
 
 
