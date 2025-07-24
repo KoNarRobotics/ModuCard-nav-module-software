4de596ae14d4a174c9777d919a92f4722493cb15
Patryk Dudizński <dodpat02@gmail.com>
2025-07-24 21:01:04 +0200

    update submodules

--- src/version.hpp
+++ src/version.hpp
@@ -2,11 +2,11 @@
 #define VERSION_HPP
 
 #define VERSION_MAJOR 0
-#define VERSION_MINOR 96
-#define VERSION_BUILD 9
+#define VERSION_MINOR 97
+#define VERSION_BUILD 0
 
 // for easy find  in a .bin file
-#define VERSION_DEF_BUILD_STRING_DEF "build_version:0.96.9"
+#define VERSION_DEF_BUILD_STRING_DEF "build_version:0.97.0"
 static const char* VERSION_DEF_BUILD_STRING = VERSION_DEF_BUILD_STRING_DEF;
 
 
